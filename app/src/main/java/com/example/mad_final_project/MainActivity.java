9410364ab3d480b57d07fb27c3d7becef4850319
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2024-11-28 10:51:31 +0000

    Added Code for database, with an included tester

--- app/src/main/java/com/example/mad_final_project/MainActivity.java
+++ app/src/main/java/com/example/mad_final_project/MainActivity.java
@@ -27,10 +27,107 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        List<HighscoreClass> highscore = db.getAllHighscore();
+
+
+        //db.emptyContacts();     // empty table if required
+        db.emptyHighscore();
+        // Inserting Contacts
+        Log.i("Insert: ", "Inserting ..");
+        db.addHighscore(new HighscoreClass("Joe", 4));
+        db.addHighscore(new HighscoreClass("Mary", 3));
+        db.addHighscore(new HighscoreClass("Jack", 8));
+        db.addHighscore(new HighscoreClass("Andrew", 5));
+        db.addHighscore(new HighscoreClass("Harold", 2));
+        db.addHighscore(new HighscoreClass("John", 9));
+
+        // Reading all contacts
+        Log.i("Reading: ", "Reading all contacts..");
+
+        for (HighscoreClass cn : highscore) {
+            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Highscore: " +
+                    cn.getHighscore();
+            Log.i("Name: ", log);
+
+        }
+
+        Log.i("divider", "====================");
+
+        HighscoreClass singleUser = db.getHighscoreClass(5);
+        Log.i("contact 5 is ", singleUser.getName());
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        int userCount = db.getHighscoreCount();
+        Log.i("User count: ", String.valueOf(userCount));
+
+
+    }
+
+    public void topFiveFilter() {
+        DatabaseHandler db = new DatabaseHandler(this);
+        List<HighscoreClass> highscoreList = db.top5Highscore();
+        Log.i("Name: ", "list successful");
+        for (HighscoreClass cn2 : highscoreList) {
+
+
+            String log = "Id: " + cn2.getID() + " ,Name: " + cn2.getName() + " ,Highscore: " +
+                    cn2.getHighscore();
+
+            Log.i("Name: ", log);
+        }
+    }
+
+    public void addHighScore(View view) {
+        String name;
+        EditText e1, e2;
+        String highscore;
+        e1 = (EditText) findViewById(R.id.nameTv);
+        e2 = (EditText) findViewById(R.id.highscoreTv);
+
+        name = String.valueOf(e1.getText());
+        highscore = String.valueOf(e2.getText());
+        DatabaseHandler db = new DatabaseHandler(this);
+        db.addHighscore(new HighscoreClass(name, Integer.parseInt(highscore)));
+        int userCount = db.getHighscoreCount();
+        Log.i("User count: ", String.valueOf(userCount));
+
+        List<HighscoreClass> highscoreList = db.getAllHighscore();
+
+
+        for (HighscoreClass cn2 : highscoreList) {
+
+
+            String log = "Id: " + cn2.getID() + " ,Name: " + cn2.getName() + " ,Highscore: " +
+                    cn2.getHighscore();
+            //if(cn2.getHighscore() >= )
+            Log.i("Name: ", log);
+        }
+        topFiveFilter();
+    }
+
+
+    //Making EditView Clear
+    public void noClickHighscore(View view)//will have to click edit view twice to clear text
+    {
+        EditText e2;
+        e2 = (EditText) findViewById(R.id.highscoreTv);
+        e2.getText().clear();
+    }
+
+    public void noClickName(View view)//will have to click edit view twice to clear text
+    {
+        EditText e1;
+        e1 = (EditText) findViewById(R.id.nameTv);
+        e1.getText().clear();
     }
 }
