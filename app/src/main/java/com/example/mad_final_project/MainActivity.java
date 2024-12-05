eb1b891b856bf7eb3d5141286a236949c95f391d
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2024-12-05 11:33:39 +0000

    Program "runs". automatically fails, cant even press the start button before it tells you "Wrong Move". It doesnt show the sequence either

--- app/src/main/java/com/example/mad_final_project/MainActivity.java
+++ app/src/main/java/com/example/mad_final_project/MainActivity.java
@@ -1,133 +1,141 @@
 package com.example.mad_final_project;
-
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener
+{
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private List<Button> playButtons;
+    private int correctIndex;
+    private boolean tiltDetected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
 
-        DatabaseHandler db = new DatabaseHandler(this);
+        playButtons = new ArrayList<>();
 
-        List<HighscoreClass> highscore = db.getAllHighscore();
+        setupPlayButtons();
 
-
-        //db.emptyContacts();     // empty table if required
-        db.emptyHighscore();
-        // Inserting Contacts
-        Log.i("Insert: ", "Inserting ..");
-        db.addHighscore(new HighscoreClass("Joe", 4));
-        db.addHighscore(new HighscoreClass("Mary", 3));
-        db.addHighscore(new HighscoreClass("Jack", 8));
-        db.addHighscore(new HighscoreClass("Andrew", 5));
-        db.addHighscore(new HighscoreClass("Harold", 2));
-        db.addHighscore(new HighscoreClass("John", 9));
-
-        // Reading all contacts
-        Log.i("Reading: ", "Reading all contacts..");
-
-        for (HighscoreClass cn : highscore) {
-            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Highscore: " +
-                    cn.getHighscore();
-            Log.i("Name: ", log);
-
-        }
-
-        Log.i("divider", "====================");
-
-        HighscoreClass singleUser = db.getHighscoreClass(5);
-        Log.i("contact 5 is ", singleUser.getName());
-
-        Log.i("divider", "====================");
-
-        // Calling SQL statement
-        int userCount = db.getHighscoreCount();
-        Log.i("User count: ", String.valueOf(userCount));
-
-
-    }
-
-    public void topFiveFilter() {
-        DatabaseHandler db = new DatabaseHandler(this);
-        List<HighscoreClass> highscoreList = db.top5Highscore();
-        Log.i("Name: ", "list successful");
-        for (HighscoreClass cn2 : highscoreList) {
-
-
-            String log = "Id: " + cn2.getID() + " ,Name: " + cn2.getName() + " ,Highscore: " +
-                    cn2.getHighscore();
-
-            Log.i("Name: ", log);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (accelerometer != null) {
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            } else {
+                Toast.makeText(this, "No Accelerometer Found", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    public void addHighScore(View view) {
-        String name;
-        EditText e1, e2;
-        String highscore;
-        e1 = (EditText) findViewById(R.id.nameTv);
-        e2 = (EditText) findViewById(R.id.highscoreTv);
+    private void setupPlayButtons() {
+        playButtons.add(findViewById(R.id.redButton));
+        playButtons.add(findViewById(R.id.blueButton));
+        playButtons.add(findViewById(R.id.greenButton));
+        playButtons.add(findViewById(R.id.yellowButton));
 
-        name = String.valueOf(e1.getText());
-        highscore = String.valueOf(e2.getText());
-        DatabaseHandler db = new DatabaseHandler(this);
-        db.addHighscore(new HighscoreClass(name, Integer.parseInt(highscore)));
-        int userCount = db.getHighscoreCount();
-        Log.i("User count: ", String.valueOf(userCount));
+        correctIndex = (int) (Math.random() * playButtons.size());
+        highlightButton(playButtons.get(correctIndex));
+    }
 
-        List<HighscoreClass> highscoreList = db.getAllHighscore();
+    private void highlightButton(Button button) {
+        button.setBackgroundColor(Color.WHITE);
+        button.postDelayed(() -> button.setBackgroundColor(getColorForButton(button)), 1000);
+    }
 
+    private int getColorForButton(Button button) {
+        if (button == findViewById(R.id.redButton)) return Color.RED;
+        if (button == findViewById(R.id.blueButton)) return Color.BLUE;
+        if (button == findViewById(R.id.greenButton)) return Color.GREEN;
+        if (button == findViewById(R.id.yellowButton)) return Color.YELLOW;
+        return Color.GRAY;
+    }
 
-        for (HighscoreClass cn2 : highscoreList) {
-
-
-            String log = "Id: " + cn2.getID() + " ,Name: " + cn2.getName() + " ,Highscore: " +
-                    cn2.getHighscore();
-            //if(cn2.getHighscore() >= )
-            Log.i("Name: ", log);
+    private void checkUserInput(int inputIndex) {
+        if (inputIndex == correctIndex) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+            restartGame();
+        } else {
+            Toast.makeText(this, "Wrong Move!", Toast.LENGTH_SHORT).show();
+            endGame();
         }
-        topFiveFilter();
     }
 
-
-    //Making EditView Clear
-    public void noClickHighscore(View view)//will have to click edit view twice to clear text
-    {
-        EditText e2;
-        e2 = (EditText) findViewById(R.id.highscoreTv);
-        e2.getText().clear();
+    private void restartGame() {
+        correctIndex = (int) (Math.random() * playButtons.size());
+        tiltDetected = false;
+        highlightButton(playButtons.get(correctIndex));
     }
 
-    public void noClickName(View view)//will have to click edit view twice to clear text
-    {
-        EditText e1;
-        e1 = (EditText) findViewById(R.id.nameTv);
-        e1.getText().clear();
+    private void endGame() {
+        Intent intent = new Intent(this, GameOverActivity.class);
+        intent.putExtra("finalScore", correctIndex);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+
+            if (!tiltDetected) {
+                if (Math.abs(x) > Math.abs(y)) {
+                    tiltDetected = true;
+                    if (x < 0) {
+                        checkUserInput(1); // Right Tilt (Blue)
+                    } else {
+                        checkUserInput(3); // Left Tilt (Yellow)
+                    }
+                } else {
+                    tiltDetected = true;
+                    if (y < 0) {
+                        checkUserInput(2); // Up Tilt (Green)
+                    } else {
+                        checkUserInput(0); // Down Tilt (Red)
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 }
